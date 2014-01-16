d202807f15fc274bb9d07a7ba37438b8b5a0be42
AlexYang <alexyang@alexyang-Lenovo-IdeaPad-P400-Touch.(none)>
2014-01-16 01:28:16 -0500

    wrote xToN.cpp. need to look at lifecycle.cpp

---
+++ CS2150/lab01/xToN.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int power(int a, int b){
+  if(b == 0) {
+    return 1;
+  }
+  else {
+    return a*power(a, b-1);
+  }
+}
+
+int main (){
+  int x,y,z;
+  cout<<"Base?";
+  cin>>x;
+  cout<<"Exponent?";
+  cin>>y;
+  z=power(x,y);
+  cout<<x<< " ^ " << y << " = " << z << endl;
+  return 0;
+}
