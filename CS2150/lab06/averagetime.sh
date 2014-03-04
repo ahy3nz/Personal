e95804ea1cf8da0a4e3c0a38f19f7d1fc4eab8b6
ahy3nz <ahy3nz@labpc14.cs.virginia.edu>
2014-03-04 12:54:05 -0500

    inlab 6

--- CS2150/lab06/averagetime.sh
+++ CS2150/lab06/averagetime.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+#Print out request for dictionary
 echo "Type exact name of dictionary file, then press enter: "
+#Read in user input for dictionary
 read dict
+#Request grid file
 echo "Type exact name of grid file, then press enter: "
+#Read in grid file
 read grid
+#initialize total time variable
 TOTAL_TIME=0
+
+#execute code 5 times
 for ((i = 1; i <=5; ++i)); do
+	#find the running time of the program by getting the last line of output
+
 	RUNNING_TIME=`./a.out $dict $grid | tail -1`
+	#add the running time to the total running tmie
 	TOTAL_TIME=$(($TOTAL_TIME + $RUNNING_TIME))
 done
+#calculate the average running time
 AVERAGE_TIME=`expr $TOTAL_TIME / 5`
+#print the average running time
 echo "$AVERAGE_TIME microseconds average time"
